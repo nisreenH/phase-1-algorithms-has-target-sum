67126ec8eec35106e6517574cf9d255394ea61e7
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2023-01-04 20:42:39 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const numbers = {};
+
+  for (const number of array) {
+    const comp = target - number;
+    if (comp in numbers) return true;
+    numbers[number] = true;
+  }
+
+  return false;
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 6, 12, 9], 15));
+  console.log("");
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
